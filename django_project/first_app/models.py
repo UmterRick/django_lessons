8973e9343563b489492ea2bcd78b5e3a06de4843
Vladyslav Usichenko <usichenko.vlad67@gmail.com>
2023-05-23 18:33:16 +0300

    create models

--- django_project/first_app/models.py
+++ django_project/first_app/models.py
@@ -1,19 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
-class Author(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    name = models.CharField(max_length=30, null=False)
-    birth_date = models.DateField(null=False)
-    def __str__(self):
-        return self.name
 
-
-class Book(models.Model):
+class Student(User):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(50, null=False)
-    rating = models.FloatField(default=3)
-    authors = models.ManyToManyField(Author, "books")
+    is_staff = False
 
-    def __repr__(self):
-        return f"{self.title} ({self.rating})"
+
+class Teacher(User):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    is_staff = True
+
+
+class Lesson(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField()
+
+
+class StudentLessonRelation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    visited = models.BooleanField(default=False)
+    comment = models.TextField()
+    points = models.IntegerField(default=0)
