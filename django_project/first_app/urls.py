3a0cb0064132876ff4ee033ac6b11bd7d29e3895
Vladyslav Usichenko <usichenko.vlad67@gmail.com>
2023-05-23 19:40:34 +0300

    refactor url

--- django_project/first_app/urls.py
+++ django_project/first_app/urls.py
@@ -4,7 +4,7 @@ from first_app import views
 
 urlpatterns = [
     path('', views.main_page),
-    path('lesson/<UUID:pk>', views.render_lesson_info, name="lesson-info"),
-    path('student/<UUID:pk>', views.render_student_profile, name="student-info"),
+    path('lesson/<uuid:pk>', views.render_lesson_info, name="lesson-info"),
+    path('student/<int:pk>', views.render_student_profile, name="student-info"),
 
 ]
