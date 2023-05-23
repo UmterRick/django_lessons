6000f7b65f55015a29e1acb585d074945419bc2f
Vladyslav Usichenko <usichenko.vlad67@gmail.com>
2023-05-23 20:50:47 +0300

    new code 2

--- django_project/first_app/models.py
+++ django_project/first_app/models.py
@@ -7,17 +7,25 @@ class Group(models.Model):
     title = models.CharField(max_length=100, null=False)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 
 class LMSUser(User):
     role = models.CharField(max_length=30, null=False, choices=[("student", "Student"), ("teacher", "Teacher")])
     group = models.ForeignKey(Group, on_delete=models.DO_NOTHING, default=None)
 
+    def __str__(self):
+        return self.email
 
 class Lesson(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100, null=False)
     description = models.TextField()
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
 
 
 class StudentLessonRelation(models.Model):
@@ -27,3 +35,6 @@ class StudentLessonRelation(models.Model):
     visited = models.BooleanField(default=False)
     comment = models.TextField()
     points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.student} - {self.lesson} {'Visited' if self.visited else 'Missed'}"
--- django_project/first_app/views.py
+++ django_project/first_app/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
-from .models import Lesson, StudentLessonRelation
+from .models import Lesson, StudentLessonRelation, Group, LMSUser
+
 
 # Create your views here.
 
 
-
-
 def main_page(request):
-    teacher = ...
-    students = []
-    return render(request=request, template_name="base.html",
+    group = Group.objects.first()
+    teacher = LMSUser.objects.get(group=group, role='teacher')
+    relations = StudentLessonRelation.objects.filter(student__group=group.pk)
+    journal = []
+    for relation in relations:
+        journal.append({relation.student: relation})
+
+    print(journal)
+
+    return render(request=request, template_name="journal.html",
                   context={"teacher": teacher,
-                           "students": students,
-                           "group_id": 1,
+                           "journal": journal,
+                           "group": group,
                            })
 
 
 def render_lesson_info(request):
     ...
 
+
 def render_student_profile(request):
     ...
-
-
-
-
