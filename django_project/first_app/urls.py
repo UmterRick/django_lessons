87dee04a90a50bf554eebfd1310a4484959fbef0
Vladyslav Usichenko <usichenko.vlad67@gmail.com>
2023-05-25 03:24:38 +0300

    new code 4

--- django_project/first_app/urls.py
+++ django_project/first_app/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path('', views.main_page),
+    path('', views.main_page, name="journal-url"),
     path('lesson/<uuid:pk>', views.render_lesson_info, name="lesson-info"),
     path('student/<int:pk>', views.render_student_profile, name="student-info"),
+    path('student/<int:student_pk>/lesson/<uuid:lesson_pk>/change-visited', views.switch_student_visited, name="switch-visited")
 
 ]
--- django_project/first_app/views.py
+++ django_project/first_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Lesson, StudentLessonRelation, Group, LMSUser
 
 
@@ -8,8 +8,8 @@ from .models import Lesson, StudentLessonRelation, Group, LMSUser
 def main_page(request):
     group = Group.objects.first()
     teacher = LMSUser.objects.get(group=group, role='teacher')
-    relations = StudentLessonRelation.objects.filter(student__group=group.pk)
-    lessons = Lesson.objects.all()
+    relations = StudentLessonRelation.objects.filter(student__group=group.pk).order_by('lesson__date')
+    lessons = Lesson.objects.all().order_by('date')
     journal = {}
     for relation in relations:
 
@@ -34,4 +34,11 @@ def render_lesson_info(request):
 def render_student_profile(request, pk):
     student = LMSUser.objects.get(pk=pk)
 
-    return HttpResponse(student)
+    return render(request=request, template_name="student.html", context={"user": student})
+
+
+def switch_student_visited(request, student_pk, lesson_pk):
+    relation = StudentLessonRelation.objects.get(student_id=student_pk, lesson_id=lesson_pk)
+    relation.visited = not relation.visited
+    relation.save()
+    return redirect(main_page)
