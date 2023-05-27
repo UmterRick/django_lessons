200469ae1dcf796164a7fbea00d8e56d0e5e427a
Vladyslav Usichenko <usichenko.vlad67@gmail.com>
2023-05-27 13:32:45 +0300

    add fixtures

--- django_project/first_app/tests.py
+++ django_project/first_app/tests.py
@@ -4,5 +4,11 @@ from django.test import TestCase
 # Create your tests here.
 class ExampleTestCase(TestCase):
 
+    def setUp(self) -> None:
+        ...
+
+    def tearDown(self) -> None:
+        ...
+
     def test_example(self):
         self.assertEqual(True, True)
